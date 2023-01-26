87aa444c8911a0a74d60e592869fa7541ceb3ee0
Naveen Kumar <textnav@gmail.com>
2023-01-26 21:45:51 +0800

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,19 +3,22 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Grid = styled.div`
+const Flex = styled.div`
   padding: 1rem;
-  display: grid;
-  grid-template-rows: 1fr 1fr 1fr;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 1rem;
-  
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  gap: 2rem;
+
   @media (max-width: 768px) {
-    grid-template-columns: 1fr;
+    flex-direction: column;
   }
 `;
 
 const Title = styled.h1`
+  font-size: 5rem;
   grid-column: 1 / 3;
   text-align: center;
   @media (max-width: 768px) {
@@ -24,24 +27,22 @@ const Title = styled.h1`
 `;
 
 const Box = styled(Link)`
-  background: rgba(255,255,255,.1);
+  background: rgba(255, 255, 255, 0.2);
   box-sizing: border-box;
   box-shadow: 0 15px 25px rgb(0 0 0 / 60%);
   border-radius: 10px;
   display: flex;
   align-items: center;
+  padding: 2rem;
   justify-content: center;
 
-  font-family: Arial, sans-serif;
-  font-size: 20px;
+  font-size: 2rem;
   text-transform: uppercase;
   text-decoration: none;
   text-transform: uppercase;
+  text-align: center;
   color: white;
-  min-height: 100px;
-  border: 3px outset #fff;
-  box-shadow: rgba(150, 150, 193, 0.25) 0px 50px 100px -20px, rgba(255, 255, 255, 0.3) 0px 30px 60px -30px, rgba(110, 137, 164, 0.35) 0px -2px 6px 0px inset;
-
+  min-height: 3rem;
   &:hover {
     cursor: pointer;
   }
@@ -49,11 +50,13 @@ const Box = styled(Link)`
 
 export function Home() {
   return (
-    <Grid>
-      <Title>Home</Title>
-      <Box to="/postMessage">Post Message</Box>
-      <Box to="/broadcastChannel">Broadcast Channel</Box>
-      <Box to="/localStorage">Local Storage</Box>
-    </Grid>
+    <>
+      <Title>Inter Tab Communication</Title>
+      <Flex>
+        <Box to="/postMessage">Post Message</Box>
+        <Box to="/broadcastChannel">Broadcast Channel</Box>
+        <Box to="/localStorage">Storage Event</Box>
+      </Flex>
+    </>
   );
 }
